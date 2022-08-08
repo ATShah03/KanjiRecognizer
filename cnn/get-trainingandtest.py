d8d5715a0bcaa0cf719a285a97f1e08bc6e68b4c
Arnav Shah <sarnav@umich.edu>
2022-08-08 15:31:20 -0400

    Update get-trainingandtest.py

--- cnn/get-trainingandtest.py
+++ cnn/get-trainingandtest.py
@@ -8,29 +8,26 @@ cols = 48
 
 kan = np.load("kanji9.npz")['arr_0'].reshape([-1, 127, 128]).astype(np.float32)
 
-print(kan.shape)
+#print(kan.shape)
 
+# Normalize values of the Kanji from 0-1
 kmax = np.max(kan)
 
-print("hi")
 kan = kan/kmax
-print("hi")
 
-train_images = np.zeros([kanji * 200, rows, cols], dtype=np.float32)
-
+# Create numpy array for labels, with each label as an integer from 0-3035
+train_images = np.zeros([kanji * 200, rows, cols], dtype=np.float32) 
 
 train_labels = np.repeat(np.arange(kanji), 200)
 
-print("hi")
-
+# Resize images to 48x48, and put them into the Train images array
 for i in range(kanji * 200):
     train_images[i] = skimage.transform.resize(kan[i], (rows, cols))
 
-print("hi")
 
 train_images, test_images, train_labels, test_labels = train_test_split(train_images, train_labels, test_size=0.2)
 
-print("hi2")
+# compress the arrays into npz files to be stored locally and used when running the model
 np.savez_compressed("train_images9.npz", train_images)
 np.savez_compressed("rain_labels9.npz", train_labels)
 np.savez_compressed("test_images9.npz", test_images)
