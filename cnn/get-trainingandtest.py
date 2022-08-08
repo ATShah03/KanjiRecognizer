2f15298bac4b43dae7f5d1080174306ac04ff40c
Arnav Shah <sarnav@umich.edu>
2022-08-08 15:27:42 -0400

    Update get-trainingandtest.py

--- cnn/get-trainingandtest.py
+++ cnn/get-trainingandtest.py
@@ -8,15 +8,29 @@ cols = 48
 
 kan = np.load("kanji9.npz")['arr_0'].reshape([-1, 127, 128]).astype(np.float32)
 
-kan = kan/np.max(kan)
+print(kan.shape)
+
+kmax = np.max(kan)
+
+print("hi")
+kan = kan/kmax
+print("hi")
 
 train_images = np.zeros([kanji * 200, rows, cols], dtype=np.float32)
 
+
 train_labels = np.repeat(np.arange(kanji), 200)
 
-      
+print("hi")
+
+for i in range(kanji * 200):
+    train_images[i] = skimage.transform.resize(kan[i], (rows, cols))
+
+print("hi")
+
 train_images, test_images, train_labels, test_labels = train_test_split(train_images, train_labels, test_size=0.2)
 
+print("hi2")
 np.savez_compressed("train_images9.npz", train_images)
 np.savez_compressed("rain_labels9.npz", train_labels)
 np.savez_compressed("test_images9.npz", test_images)
